2e95f417682393f982fc2ea24349932644ae4a31
Webpoint <webpoint@Webpoints-Mac-mini.local>
2023-04-15 18:55:54 +0545

    header build

---
+++ src/app/layout.jsx
@@ -0,0 +1,18 @@
+import Header from '@/components/Header'
+import './globals.css'
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        {/* Header */}
+        <Header />
+        {/* Navbar */}
+        
+        {/* SearchBox */}
+
+        {children}
+      </body>
+    </html>
+  )
+}
